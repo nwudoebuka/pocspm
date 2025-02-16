de057eedabfa35d874546531242e4dbd65e0e139
Anthony <anthony@AP24DKM070.local>
2025-02-16 02:01:54 +0100

    WIP: frameworkbinary poc

--- Package.swift
+++ Package.swift
@@ -40,16 +40,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Appvestor_iOS_Stats",
+    name: "AppvestorStatsFramework",
     platforms: [.iOS(.v12)],
     products: [
         .library(
-            name: "Appvestor_iOS_Stats",
-            targets: ["Appvestor_iOS_Stats"]),
+            name: "AppvestorStatsFramework",
+            targets: ["AppvestorStatsFramework"]),
     ],
     targets: [
         .binaryTarget(
-            name: "Appvestor_iOS_Stats",
+            name: "AppvestorStatsFramework",
             url: "https://github.com/nwudoebuka/pocspm/releases/download/1.0.0/AppvestorStatsFramework.xcframework.zip",
             checksum: "2271d795ae691658c9ca4e54d366b67fc4cde10f2c838d9508b37f3a7b9d7d26"
         ),
